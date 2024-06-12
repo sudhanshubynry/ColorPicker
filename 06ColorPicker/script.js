ae43d0fdf9060c9f68391a3f12fa22b119809d49
Sudhanshu Kumar <56245722+sudhanshubynry@users.noreply.github.com>
2024-06-12 14:43:34 +0530

    Add files via upload

---
+++ 06ColorPicker/script.js
@@ -0,0 +1,37 @@
+
+// console.log(document.getElementById("colorPickerContainer").style.backgroundColor = "hotpink");
+
+function buttonOne(){
+    document.getElementById("colorPickerContainer").style.backgroundColor="#e0e0e0";
+    let x = document.getElementById("selectedColorHexCode").textContent = "#e0e0e0";
+    // console.log(x);
+
+}
+
+function buttonTwo(){
+    
+    document.getElementById("colorPickerContainer").style.backgroundColor="#6fcf97";
+    let x = document.getElementById("selectedColorHexCode").textContent = "#6fcf97";
+    // console.log(x);
+
+
+
+}
+
+function buttonThree(){
+    
+    document.getElementById("colorPickerContainer").style.backgroundColor="#56ccf2";
+    let x = document.getElementById("selectedColorHexCode").textContent = "#56ccf2";
+    // console.log(x);
+
+
+}
+
+function buttonFour(){
+    
+    document.getElementById("colorPickerContainer").style.backgroundColor="#bb6bd9";
+    let x = document.getElementById("selectedColorHexCode").textContent = "#bb6bd9";
+    // console.log(x);
+
+
+}
